2457051b626be752ed1665268bddd9c3cdae515f
Adam Tuor <tuora99@gmail.com>
2023-01-08 02:14:51 +0900

    got average change working

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import csv
 totalMonths = 0
 totalPL = 0
 changePL = 0
+previousPL = 0
 averagePL = 0
 
 #these need to be date and amount
@@ -21,7 +22,24 @@ with open(fileName) as csvfile:
         totalMonths = totalMonths +1
 
         totalPL= totalPL + int(row[1])
+        
+        if totalMonths <= 1:
+            previousPL = int(row[1])
+        else:
+            changePL =  changePL+ (int(row[1]) - previousPL)
+            previousPL = int(row[1])
 
-    print(totalPL)
-    #print(totalMonths)
+        #changePL= changePL + (int(row+1[1]) - int(row[1]))
+
+    #how do I get the first month in
+    #start on b3-b2=>++
+    #add up all values and divide by 85.  first month x change so not 86
+    
+    #print(f"test {changePL}")
+    print(f"Total Months: {totalMonths}")
+    print(f"Total: ${totalPL}")
+    print(f"Average Change: ${changePL/(totalMonths-1)}")
+    print(f"Greatest Increase in Profits: ")  
+    print(f"Greatest Decrease in Profits: ")   
+   
         
